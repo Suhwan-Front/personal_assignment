2207373d4d97a6483c6d3516e72c07cdfd1af63b
Suhwan0818 <tnghks1997@gmail.com>
2023-06-12 18:11:38 +0900

    ♻️ main refactoring

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -2,9 +2,9 @@ import {useEffect} from 'react';
 import {Link, useLocation, useNavigate} from 'react-router-dom';
 
 const Main = () => {
-  const isLogin = localStorage.getItem('jwt') !== null;
   const navigate = useNavigate();
   const location = useLocation();
+  const isLogin = localStorage.getItem('jwt') !== null;
 
   const handleLogout = () => {
     localStorage.removeItem('jwt');
@@ -13,17 +13,18 @@ const Main = () => {
 
   useEffect(() => {
     const jwt = localStorage.getItem('jwt');
-    console.log(`로컬 스토리지 저장 : ${jwt}`);
-  });
+    console.log(`로컬 스토리지 저장: ${jwt}`);
+  }, []);
 
   useEffect(() => {
     if (
       isLogin &&
-      (location.pathname === 'signin' || location.pathname === 'signup')
+      (location.pathname === '/signin' || location.pathname === '/signup')
     ) {
       navigate('/');
     }
   }, [isLogin, location.pathname, navigate]);
+
   return (
     <>
       {isLogin ? (
@@ -40,23 +41,20 @@ const Main = () => {
             <div>로그인 페이지로 이동</div>
           </Link>
           <div>경로 접속시 리다이렉션 확인용</div>
-          <Link to="/todo">
-            <div>투두리스트 페이지로 이동</div>
-          </Link>
-        </>
-      )}
-      {location.pathname === '/' && isLogin && (
-        <>
-          <Link to="/todo">
-            <div>투두리스트 페이지로 이동</div>
-          </Link>
-          <div>경로 접속시 리다이렉션 확인용</div>
-          <Link to="/signup">
-            <div>회원가입 페이지로 이동</div>
-          </Link>
-          <Link to="/signin">
-            <div>로그인 페이지로 이동</div>
-          </Link>
+          {location.pathname === '/' && (
+            <>
+              <Link to="/todo">
+                <div>투두리스트 페이지로 이동</div>
+              </Link>
+              <div>경로 접속시 리다이렉션 확인용</div>
+              <Link to="/signup">
+                <div>회원가입 페이지로 이동</div>
+              </Link>
+              <Link to="/signin">
+                <div>로그인 페이지로 이동</div>
+              </Link>
+            </>
+          )}
         </>
       )}
     </>
